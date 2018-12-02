0014aa3440327d3db632a38b442e0c0516acaebd
Wexaris <wexaris@gmail.com>
2018-12-03 00:37:16 +0200

    Added newline saving to the Translation Unit This will make finding lines for the error reporter much easier

--- src/bootstrap/source/translation_unit.cpp
+++ src/bootstrap/source/translation_unit.cpp
@@ -1,5 +1,39 @@
 #include "translation_unit.hpp"
 #include "util/ranges.hpp"
+#include "errors/handler.hpp"
+
+TranslationUnit::TextPos TranslationUnit::pos_from_index(size_t index) const {
+
+	if (newlines.empty() || index < newlines[0])
+		return TextPos{0, index};
+
+	TextPos pos;
+
+	// Find the line that the index is from
+	for (size_t i = 0; i < newlines.size(); i++) {
+		if (i == newlines.size())
+			pos.line = i;
+		else if (newlines[i] > index)
+			pos.line = i - 1;
+		pos.col = index - newlines[pos.line];
+	}
+
+	auto err = ErrorHandler::new_error(ErrSeverity::BUG, "failed to find the line that character " + std::to_string(index) + " is on");
+	Emitter::emit(err);
+	return pos;
+}
+
+std::string TranslationUnit::get_line(uint ln) const {
+
+	if (newlines.empty())
+		return src;
+
+	size_t i = 1;
+	while (src[newlines[ln] + i] != '\n')
+		i++;
+
+	return src.substr(newlines[ln], i);
+}
 
 std::string TranslationUnit::this_source_line(size_t index) const {
 	// Declate a start index
@@ -32,5 +66,5 @@ std::string TranslationUnit::this_source_line(size_t index) const {
 	while (range::is_whitespace(src[index]))
 		index--;
 
-	return src.substr(start, index - start);
+	return src.substr(start, index - start + 1);
 }
--- src/bootstrap/source/translation_unit.hpp
+++ src/bootstrap/source/translation_unit.hpp
@@ -16,6 +16,10 @@ private:
 	/* This Translation Unit's start position in the CodeMap */
 	size_t start_position;
 
+	/* The positions of all of the newline markers in the source code.
+	 * Used for getting positions quickly. */
+	std::vector<size_t> newlines;
+
 public:
 	explicit TranslationUnit(const std::string& src) 
 		: src(src) {}
@@ -23,21 +27,32 @@ public:
 	TranslationUnit(const std::string& path, const std::string& src, size_t start_pos) 
 		: path(path), src(src), start_position(start_pos) {}
 
+	struct TextPos {
+		size_t line;
+		size_t col;
+	};
+
+	std::string this_source_line(size_t index) const;
+
 	/* Returns the line of source that the index was from.
 	 * Beginning and ending whitespace is removed. */
-	std::string this_source_line(size_t index) const;
+	TextPos pos_from_index(size_t index) const;
+
+	std::string get_line(uint ln) const;
+
+	/* Returns the line of source that the index was from.
+	 * Beginning and ending whitespace is removed. */
+	void save_newline(size_t index) {
+		newlines.push_back(index);
+	}
 
 	/* Returns the path to Translation Unit. */
 	inline const std::string& filepath() const	{ return path; }
+	/* A view into the file's source code. */
+	inline std::string_view source() const		{ return src; }
 
 	/* Start position in the CodeMap. */
 	inline int start_pos() const				{ return start_position; }
 	/* End position in the CodeMap. */
 	inline size_t end_pos() const				{ return start_position + src.length(); }
-
-	/* A view into the file's source code. */
-	inline std::string_view source() const		{ return src; }
-
-	/* Length of the source code in the file. */
-	inline size_t len() const					{ return src.length(); }
 };
