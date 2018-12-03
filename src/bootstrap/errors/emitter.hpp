45f0667554f8c3207ce14f3a6d6f12bdc17ce923
Wexaris <wexaris@gmail.com>
2018-12-03 19:55:54 +0200

    Made fatal errors fatal in the Emitter

--- src/bootstrap/errors/emitter.hpp
+++ src/bootstrap/errors/emitter.hpp
@@ -2,10 +2,16 @@
 #include <string>
 #include "error.hpp"
 
+/* A small wrapper around formatting and printing error strings. */
 class Emitter {
 
 public:
+	/* Emit the given 'Error'.
+	 * The error's 'format()' is called to get the final string that will be printed. */
 	static void emit(const Error& err) {
 		printf(err.format().c_str());
+		
+		if (err.is_fatal())
+			throw std::exception();
 	}
 };
