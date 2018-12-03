0a5ab460369d29449e0262ddee25ac3356aed4de
Wexaris <wexaris@gmail.com>
2018-12-04 00:52:22 +0200

    Fixed bugs in line getting 'pos_from_index()' used to return newline indices, not line numbers 'get_line()' could return everything starting from the given line

--- src/bootstrap/source/translation_unit.cpp
+++ src/bootstrap/source/translation_unit.cpp
@@ -4,6 +4,7 @@
 
 TranslationUnit::TextPos TranslationUnit::pos_from_index(size_t index) const {
 
+	// File has no newlines or it hasn't been entirely lexed
 	if (newlines.empty() || index < newlines[0])
 		return TextPos{0, index};
 
@@ -13,11 +14,13 @@ TranslationUnit::TextPos TranslationUnit::pos_from_index(size_t index) const {
 	for (size_t i = 0; i <= newlines.size(); i++) {
 		if (i == newlines.size()) {
 			pos.line = i;
-			pos.col = index - newlines[pos.line];
+			pos.col = index - newlines[pos.line-1];
 			return pos;
 		}
 		else if (newlines[i] > index) {
-			pos.line = i - 1;
+			pos.line = i;
+			for (auto e : newlines)
+				printf("%lu\n", e);
 			pos.col = index - newlines[pos.line];
 			return pos;
 		}
@@ -28,13 +31,18 @@ TranslationUnit::TextPos TranslationUnit::pos_from_index(size_t index) const {
 	return pos;
 }
 
-std::string TranslationUnit::get_line(uint ln) const {
+std::string TranslationUnit::get_line(size_t ln) const {
 
+	// File has no newlines or it hasn't been entirely lexed
 	if (newlines.empty())
 		return src;
 
+	// We know when the next line begins
+	if (newlines.size() > ln)
+		return src.substr(newlines[ln - 1], newlines[ln] - newlines[ln - 1] - 1);
+
 	size_t i = 1;
-	while (src[newlines[ln - 1] + i] != '\n')
+	while (src.length() > newlines[ln - 1] + i && src[newlines[ln - 1] + i] != '\n')
 		i++;
 
 	return src.substr(newlines[ln - 1], i);
