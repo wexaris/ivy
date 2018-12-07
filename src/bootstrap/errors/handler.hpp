6220caf916f8f8fe03ab51f63d610e4d6e9a53bf
Wexaris <wexaris@gmail.com>
2018-12-08 00:49:56 +0200

    Added a few error wrappers to the ErrorHandler

--- src/bootstrap/errors/handler.hpp
+++ src/bootstrap/errors/handler.hpp
@@ -41,4 +41,27 @@ public:
 
 	/* True is there have been any errors. */
 	inline bool has_errors() const			{ return err_count > 0; }
+
+	/* True is there have been any errors. */
+	inline Error fatal_higligted(const std::string& msg, const Span& sp) {
+		auto err = new_error(FATAL, msg);
+		err.add_span(sp);
+		err.add_highlight();
+		return err;
+	}
+
+	/* Creates and emits an internal compiler failure error message.
+	 * Will be fatal. */
+	inline void emit_fatal_bug(const std::string& msg) {
+		auto err = new_error(BUG, msg);
+		err.emit();
+	}
+
+	/* Creates and emits an internal compiler failure error message about
+	 * a missing feature.
+	 * Will be fatal.
+	 * A wrapper around 'fatal_bug()'. */
+	inline void emit_fatal_unimpl(const std::string& msg) {
+		emit_fatal_bug(msg + " not implemented yet");
+	}
 };
