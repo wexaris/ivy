126b7dc7dbc62046ae0b405ae2f8441447bfa488
Wexaris <wexaris@gmail.com>
2018-12-08 00:14:33 +0200

    Removed Sesssion usage in test cases

--- src/bootstrap/tests/lexer_tests.cpp
+++ src/bootstrap/tests/lexer_tests.cpp
@@ -15,15 +15,14 @@ namespace tests {
 
 			// Check for correct absolute position
 			if (tk.span().lo.bit == 1 || tk.span().hi.bit == 5) {
-				Session::msg("COMPLETED token_has_correct_absolute_pos");
+				printf("COMPLETED token_has_correct_absolute_pos\n");
 				return;
 			}
 
 			// The lexer has assigned the wrong position
 			// Either location tracking is broken,
 			// or some token's positon isn't being set correctly
-			Session::err("FAILED token_has_correct_location; the token's absolute position was wrong (" \
-						+ std::to_string(tk.span().lo.bit) + "," + std::to_string(tk.span().hi.bit) + ")");
+			printf("FAILED token_has_correct_location; the token's absolute position was wrong (%lu, %lu)\n", tk.span().lo.bit, tk.span().hi.bit);
 		}
 
 		void token_has_correct_line_pos() {
@@ -38,15 +37,14 @@ namespace tests {
 
 			// Check for correct line position
 			if (tk.span().lo.line == 2 || tk.span().hi.line == 2) {
-				Session::msg("COMPLETED token_has_correct_line_pos");
+				printf("COMPLETED token_has_correct_line_pos\n");
 				return;
 			}
 			
 			// The lexer has assigned the wrong position
 			// Either location tracking is broken,
 			// or some token's positon isn't being set correctly
-			Session::err("FAILED token_has_correct_line_pos; the token's line position was wrong (" \
-						+ std::to_string(tk.span().lo.line) + "," + std::to_string(tk.span().hi.line) + ")");
+			printf("FAILED token_has_correct_line_pos; the token's line position was wrong (%lu, %lu)\n", tk.span().lo.line, tk.span().hi.line);
 		}
 
 		void token_has_correct_column_pos() {
@@ -61,15 +59,14 @@ namespace tests {
 
 			// Check for correct column position
 			if (tk.span().lo.col == 1 || tk.span().hi.col == 5) {
-				Session::msg("COMPLETED token_has_correct_column_pos");
+				printf("COMPLETED token_has_correct_column_pos\n");
 				return;
 			}
 			
 			// The lexer has assigned the wrong position
 			// Either location tracking is broken,
 			// or some token's positon isn't being set correctly
-			Session::err("FAILED token_has_correct_column_pos; the token's column position was wrong (" \
-						+ std::to_string(tk.span().lo.col) + "," + std::to_string(tk.span().hi.col) + ")");
+			printf("FAILED token_has_correct_column_pos; the token's column position was wrong (%lu, %lu)\n", tk.span().lo.col, tk.span().hi.col);
 		}
 
 		void return_eof_without_translation_unit() {
@@ -82,15 +79,14 @@ namespace tests {
 
 			// Return if the token is 'END'
 			if (lex.next_token() == TokenType::END) {
-				Session::msg("COMPLETED eof_without_translation_unit");
+				printf("COMPLETED eof_without_translation_unit\n");
 				return;
 			}
 
 			// The lexer retrieved a non-'END' token even though there is no source code
 			// The lexer should have checked if the current token is valid before calling 'next_token_inner()', and,
 			// since ir wouldn't be, it should have assumed there was no more text and returned an 'END' token.
-			Session::err("FAILED return_eof_without_translation_unit; Lexer retrieved a valid token without a valid Translation Unit (" \
-						+ translate::tk_type(tk) + ")");
+			printf("FAILED return_eof_without_translation_unit; Lexer retrieved a valid token without a valid Translation Unit (%s)\n", translate::tk_type(tk).c_str());
 		}
 	}
 }
