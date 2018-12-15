dbe85a1d3a2b4a4896da9386493cee307b0fdc5d
Wexaris <wexaris@gmail.com>
2018-12-15 23:23:54 +0200

    Added WideSpan that also stores line and column positions

--- src/bootstrap/util/span.hpp
+++ src/bootstrap/util/span.hpp
@@ -32,3 +32,31 @@ struct Span {
 	 * The position is not stored in the Span, rather it is searched for in the TU. */
 	TextPos hi_textpos() const { return tu->pos_from_index(hi_bit); }
 };
+
+/* A large position in a file.
+ * Stores a pointer to the Translation Unit of origin,
+ * the absolute start and end positions in the file, as well as
+ * the line and column of the start and end. */
+struct WideSpan {
+	/* The Translation Unit of the span's origin. */
+	const TranslationUnit* tu;
+
+	struct FilePos {
+		size_t bit;
+		size_t line;
+		size_t col;
+	} lo, hi;
+
+	WideSpan(const TranslationUnit& tu, size_t lo_bit, size_t lo_line, size_t lo_col, size_t hi_bit, size_t hi_line, size_t hi_col)
+		: tu(&tu),
+		lo({lo_bit, lo_line, lo_col}),
+		hi({hi_bit, hi_line, hi_col})
+	{}
+
+	/* Finds the line and column that the Span starts at.
+	 * The position is not stored in the Span, rather it is searched for in the TU. */
+	TextPos lo_textpos() const { return TextPos{lo.line, lo.col}; }
+	/* Finds the line and column that the Span ends at.
+	 * The position is not stored in the Span, rather it is searched for in the TU. */
+	TextPos hi_textpos() const { return TextPos{hi.line, hi.col}; }
+};
